36a0700154409b4c7318f810dcd3769189d04801
Bert Temme <btemme@hotmail.com>
2024-11-12 12:37:27 +0100

    add testcase

---
+++ src/Accord.MachineLearning.Demo/Accord.MachineLearning.Demo/Instance.cs
@@ -0,0 +1,24 @@
+﻿
+public class Instance
+{
+    public Instance()
+    {
+        Scale = 1;
+    }
+    public Wkx.Geometry Position { get; set; }
+
+    public double Scale { get; set; }
+
+    public double[] ScaleNonUniform { get; set; }
+
+    public double Rotation { get; set; }
+
+    public Array Tags { get; set; }
+
+    public object Model { get; set; }
+
+    public double Yaw { get; set; }
+
+    public double Pitch { get; set; }
+    public double Roll { get; set; }
+}
